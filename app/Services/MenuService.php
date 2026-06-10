ee59ef73a4e3d005ff8d465e9fcc7ccf3fc88faf
Meep <acduong4567@gmail.com>
2026-06-10 18:31:15 +0700

    đã có bug web die =))

--- app/Services/MenuService.php
+++ app/Services/MenuService.php
@@ -30,13 +30,28 @@ class MenuService
             ->toArray();
     }
 
-    public function tree(array $menus, ?int $parentId = null): array
+    public function tree(array $menus, ?int $parentId = null, array $visitedIds = [], int $depth = 0): array
     {
+        if ($depth > 20) {
+            return [];
+        }
+
         return collect($menus)
             ->where('parent_id', $parentId)
             ->sortBy('sort_order')
-            ->map(function ($menu) use ($menus) {
-                $menu['children'] = $this->tree($menus, $menu['id']);
+            ->reject(function ($menu) use ($visitedIds) {
+                $id = data_get($menu, 'id');
+
+                return ! is_int($id) && ! ctype_digit((string) $id)
+                    ? false
+                    : in_array((int) $id, $visitedIds, true);
+            })
+            ->map(function ($menu) use ($menus, $visitedIds, $depth) {
+                $id = (int) data_get($menu, 'id', 0);
+                $nextVisitedIds = $id > 0 ? [...$visitedIds, $id] : $visitedIds;
+
+                $menu['children'] = $this->tree($menus, $id > 0 ? $id : null, $nextVisitedIds, $depth + 1);
+
                 return $menu;
             })
             ->values()
